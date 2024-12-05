ae6ad9a8517d6a122a389611608e3a0050a56b0d
Md Nahid Hassan <mdnahidhassan01@gmail.com>
2024-12-05 21:17:23 +0600

    product api implemented

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\ProductController;
 use App\Http\Controllers\Backend\StoreController;
 use App\Http\Controllers\Backend\SupplierController;
 use App\Http\Middleware\AuthMiddleware;
@@ -22,5 +23,9 @@ Route::middleware([AuthMiddleware::class])->group(function () {
     Route::post('/store/create', [StoreController::class, 'createStore'])->name('store.create');
     Route::post('/store/edit', [StoreController::class, 'editStore'])->name('store.update');
     Route::delete('/store/delete/{storeId}', [StoreController::class, 'deleteStore'])->name('store.delete');
+
+    Route::post('/product/create', [ProductController::class, 'createProduct'])->name('product.create');
+    Route::post('/product/edit', [ProductController::class, 'editProduct'])->name('product.update');
+    Route::delete('/product/delete/{productId}', [ProductController::class, 'deleteProduct'])->name('product.delete');
 
 });
