6be91ca8c3fb5d513361edea908c671f2474033c
Md Nahid Hassan <NahidHassanOfficial@users.noreply.github.com>
2024-12-08 16:07:24 +0600

    updated AuthRedirect middleware (#26)

--- app/Http/Middleware/AuthRedirectMiddleware.php
+++ app/Http/Middleware/AuthRedirectMiddleware.php
@@ -19,6 +19,13 @@ class AuthRedirectMiddleware
         if (!$token) {
             $bearerToken = $request->header('Authorization');
             if (!$bearerToken) {
+
+                //if no token exist then let the request pass to next if route name matches
+                $requestedRoute = $request->route()->getName();
+                if ($requestedRoute == 'login' || $requestedRoute == 'register') {
+                    return $next($request);
+                }
+
                 return redirect(route('login'));
             }
             $token = str_replace('Bearer ', '', $bearerToken);
@@ -28,6 +35,13 @@ class AuthRedirectMiddleware
         if ($result == "unauthorized") {
             return redirect(route('login'));
         } else {
+
+            //if current route equal to login or register then redirect to dashboard
+            $requestedRoute = $request->route()->getName();
+            if ($requestedRoute == 'login' || $requestedRoute == 'register') {
+                return redirect(route('dashboard'));
+            }
+
             return $next($request);
         }
 
