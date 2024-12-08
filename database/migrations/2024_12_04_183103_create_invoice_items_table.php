97381adbfec8cd247c5c3938fb69e5176d02b445
Md Nahid Hassan <NahidHassanOfficial@users.noreply.github.com>
2024-12-08 11:47:11 +0600

    updated foreign key constrained

--- database/migrations/2024_12_04_183103_create_invoice_items_table.php
+++ database/migrations/2024_12_04_183103_create_invoice_items_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('invoice_items', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('invoice_id')->constrained()->onDelete('cascade');
-            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('invoice_id')->constrained()->onDelete('restrict');
+            $table->foreignId('product_id')->constrained()->onDelete('restrict');
             $table->string('name');
             $table->integer('quantity');
             $table->decimal('amount', 10, 2);
